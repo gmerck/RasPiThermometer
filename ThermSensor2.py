bf7f3f12f13e706b49f06b4b2a0ff40819ff60d7
gmerck <gmerck@gmail.com>
2015-12-03 09:20:47 -0500

    Change sensorID to variable

--- ThermSensor2.py
+++ ThermSensor2.py
@@ -1,5 +1,6 @@
 from w1thermsensor import W1ThermSensor
+sensorID = "031502ee7fff"
 
-sensor1 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "031502ee7fff")
+sensor1 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, sensorID )
 temperature_in_fahrenheit = sensor1.get_temperature(W1ThermSensor.DEGREES_F)
 print(temperature_in_fahrenheit)
